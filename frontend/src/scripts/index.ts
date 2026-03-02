c4d5f8f966b67abfbb1f3b7fd0dc0ab5a6ab4655
Anish Kumar <akumar@techrayslabs.com>
2026-03-02 17:03:23 +0530

    feat: Added Navbar Component

--- frontend/src/scripts/index.ts
+++ frontend/src/scripts/index.ts
@@ -7,7 +7,8 @@ import { sidebar } from "./sidebar.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
 
-  await loadComponent("sidebar-placeholder", "/frontend/src/components/sidebar.html");
+  await loadComponent("sidebar-placeholder", "./src/components/sidebar.html");
+  await loadComponent("sidebar-navbar", "./src/components/navbar.html");
 
   sidebar();
 
